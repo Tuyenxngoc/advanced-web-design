ecb331e84c8314c8e967803e752e02c3e59a40ca
tuanntrieu <trieudangtuan18032003@Gmail.com>
2024-06-02 00:24:38 +0700

    Add logout, fix navbar

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,14 @@ $(function () {
   $("#header").load("../components/header.html");
   $("#navbar").load("../components/navbar.html");
 });
+
+$(document).ready(function() {
+  $(document).click(function(event) {
+      var $target = $(event.target);
+      var $logoutElement = $('#logout-id');
+
+      if (!$target.closest('#user-dropdown').length && $logoutElement.hasClass('show')) {
+          $logoutElement.collapse('hide');
+      }
+  });
+});
