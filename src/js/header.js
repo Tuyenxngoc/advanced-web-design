e5787f7d6b13395ffc9028917a63cc427c085c68
tuanntrieu <trieudangtuan18032003@Gmail.com>
2024-06-05 20:51:01 +0700

    Fix display username

--- src/js/header.js
+++ src/js/header.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     // Lấy tên người dùng từ localStorage
-    var username = localStorage.getItem('username');
+    var displayName = localStorage.getItem('username');
 
     // Kiểm tra nếu có tên người dùng trong localStorage thì cập nhật nội dung của phần tử #username
-    if (username) {
-        $('#username').text(username);
+    if (displayName) {
+        $('#displayName').text(displayName);
     }
 });
